cf3ea342ed527c65193187a56e821ad94a7a086e
Bhuvana KA <bhuvanaka@bhuvana.local>
2021-07-13 15:41:29 +0530

    edited for single file download

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 PyDrive to download from Google drive to a remote machine 
 
+note - creds.json file is created automatically when u first run quickstart.py
+
+
 Read Blog at - https://link.medium.com/5LJn8vBxt6
 for detailed information.
 
--- quickstart.py
+++ quickstart.py
@@ -4,13 +4,25 @@ gauth = GoogleAuth()
 gauth.CommandLineAuth()
 drive = GoogleDrive(gauth)
 # To list all files in your google drive at the root folder
-file_list1 = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 
-# To list asll files in a particular folder. (***)
-#file_list1 = drive.ListFile({'q': "'{ID of the file}' in parents and trashed=false"}).GetList()
+# to list all files in the google drive root
+for file in file_list:
+   print('title: %s, id: %s' % (file['title'], file['id']))
+# If you want to download a specfic file, you can get the id of the file in the drive using the code in line 10 -11 . Once u have got the id of the file, you can 
+# To list all files in a particular folder. (***)
+file1 = drive.ListFile({'q': "'{ID of the file}' in parents and trashed=false"}).GetList()
 
-for file1 in file_list1:
-   print('title: %s, id: %s' % (file1['title'], file1['id']))
-   #file1.GetContentFile(file1['title'])
+# to download all files in a folder . Here file1 is the folder that we want to download
+for file in file1:
+   print('title: %s, id: %s' % (file['title'], file['id']))
+   file1.GetContentFile(file1['title'])
+
+
+# if you want to specifically download a single file . Get the file id using the code in lines 10 -11
+fileID = '<enter the file id here>'
+filetodownload = drive.CreateFile({'id':fileID})
+filetodownload.GetContentFile('<enter the name that u want to use for the downloaded file here>')
+print("done")
 
 
